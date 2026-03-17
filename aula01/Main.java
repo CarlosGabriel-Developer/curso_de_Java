09f265af7c097279edc4b9cf6637e133e6de5916
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-03-17 14:50:23 -0300

    Primeiro commit

--- aula01/Main.java
+++ aula01/Main.java
@@ -2,7 +2,8 @@ package aula01;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ola mundo");
+        System.out.println("olá, mundo");
+        
     }
     
 }
