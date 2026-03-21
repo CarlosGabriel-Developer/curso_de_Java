82b81c6616ab48aec85de5671a64be43509832b3
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-03-21 15:54:36 -0300

    Operadores Aritméticos e Classe Math

---
+++ aula04OperadoresAritmeticos/Main.java
@@ -0,0 +1,24 @@
+package aula04OperadoresAritmeticos;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+    //==================================================
+    // OPERADORES DE ATRIBUIÇÃO
+    /*
+    += soma e atribuir
+    -= subrair e atribuir
+    *= multiplica e atribuir
+    /= dividi e atribuir
+    %= resto e atribuir
+    
+    
+    */
+
+    
+
+
+    }
+
+}
