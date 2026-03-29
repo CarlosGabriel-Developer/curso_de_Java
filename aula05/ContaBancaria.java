ca765d7a7efb301102ac7812059aec5865cd728a
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-03-29 12:02:35 -0300

    Classe conta bancaria

---
+++ aula05/ContaBancaria.java
@@ -0,0 +1,34 @@
+package aula05;
+
+public class ContaBancaria {
+
+    private double saldo;
+
+    public ContaBancaria(double saldoInical) { 
+        this.saldo = saldoInical;
+    }
+    
+    public void depositar(double valor) {
+        if (valor > 0){
+            saldo += valor;
+            System.out.printf("Deposíto realizado,No valor de %.2f%n",valor);
+        }
+    }    
+
+    public void saque (double valor) { 
+        if (valor > 0 && valor <= saldo){
+            saldo -= valor;
+            System.out.printf("Saque realizado,Saldo atual da sua conta é de %d%n",saldo);
+        } else { 
+            System.out.printf("Saque invalido%n");
+        }
+
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    
+    
+}
