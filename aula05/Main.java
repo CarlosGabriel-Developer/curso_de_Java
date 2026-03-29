ce6a95e6fc5b220c46f5f8ed99e9daae1640c55e
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-03-29 12:11:09 -0300

    Add classe main

---
+++ aula05/Main.java
@@ -0,0 +1,18 @@
+package aula05;
+
+public class Main { 
+
+    public static void main(String[] args) {
+        
+
+        ContaBancaria conta = new ContaBancaria(0);
+
+
+        conta.depositar(5000);
+        conta.depositar(400);
+        
+  
+    }
+
+
+}
