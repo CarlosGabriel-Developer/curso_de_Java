1e3ba25567f7a6c576a6374a07abd799c09215ab
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-03-29 12:28:22 -0300

    remove

--- aula04/Main.java
+++ aula04/Main.java
@@ -7,7 +7,7 @@ public class Main {
         //criando o objeto
         Carro opala = new Carro ("Marrom","SS",1978,0f);
         Carro ford = new Carro("azul", "Mustang", 1977, null);
-        Carro ferrari = new Carro("Vermelho", "Puro-Sangue", 2022, null);
+        
 
         opala.acelerar(70);
         opala.acelerar(20);
@@ -15,7 +15,7 @@ public class Main {
 
         ford.acelerar(130);
 
-        ferrari.acelerar(140);
+        
 
     }
 }
