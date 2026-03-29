26b2cea69271b44a0246e3f1cb736761e3b0a2a1
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-03-29 12:26:36 -0300

    add

--- aula05/Main.java
+++ aula05/Main.java
@@ -6,12 +6,15 @@ public class Main {
         
 
         ContaBancaria conta = new ContaBancaria(0);
+        ContaBancaria conta2 = new ContaBancaria(50000);
 
 
         conta.depositar(5000);
         conta.depositar(400);
 
         conta.saque(2400);
+
+        conta2.saque(4000);
 
         
 
