89a37fc366658f84bfd39f3319c4431a88d7f4b4
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-05-01 18:40:19 -0300

    add Estrutura sequencia basica

---
+++ aula04/Main.java
@@ -0,0 +1,31 @@
+package aula04;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    Scanner javascan = new Scanner(System.in);
+
+    System.out.println("Digite a primeira nota : ");
+    Float nota1 = javascan.nextFloat();
+
+    System.out.println("Digte a segunda nota : ");
+    Float nota2 = javascan.nextFloat();
+
+    System.out.println("Digite a terceira nota : ");
+    Float nota3 = javascan.nextFloat();
+
+    System.out.println("Digite o quarto valor : ");
+    Float nota4 = javascan.nextFloat();
+
+    Float soma_notas = (nota1+nota2+nota3+nota4) ;
+    Float media_notas = soma_notas /4 ;
+
+    System.out.printf("A media das suas notas é de : [ %.2f ] ",media_notas);
+
+
+
+
+    }
+}
